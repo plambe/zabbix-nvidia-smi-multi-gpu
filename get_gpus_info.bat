7f116de1bb6f8e61fd0dbdd758e88390f28ed404
plambe <plambeto@gmail.com>
2018-02-14 01:53:01 +0200

    A Windows batch version of the LLD script
    As requested here: https://github.com/plambe/zabbix-nvidia-smi-multi-gpu/issues/1
    
    I don't have a Windows machine with multiple nvidia gpus so I am unable to properly test it at the moment.

---
+++ get_gpus_info.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+echo {
+echo "data":[
+
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`"C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" -L`) DO (
+    if !count! GTR 1 echo ,
+
+    SET line=%%F
+    SET var!count!=%%F
+    SET /a count=!count!+1
+    for /f "tokens=1 delims=:" %%A in ('echo %%F') do (
+        for /f "tokens=2 delims= " %%B in ('echo %%A') do (
+            echo|set /p={"{#GPUINDEX}":"
+            echo|set /p=%%B", "
+        )
+    )
+    for /f "tokens=3 delims=:" %%A in ('echo %%F') do (
+        echo|set /p={#GPUUUID}":"
+        for /f "tokens=1 delims= " %%B in ('echo %%A') do (
+            for /f "tokens=1 delims=)" %%C in ('echo %%B') do (
+                echo|set /p=%%C"}
+            )
+        )
+        rem echo|set /p=
+    )
+)
+echo.
+echo ]
+echo }
+
+ENDLOCAL
+
